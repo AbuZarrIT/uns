9ab9fc50474156d07c165c2de0518d443d7f4122
AbuZarrIT <AbuZarrIT>
2022-12-28 14:52:01 +0100

    сделал меню

--- src/index.js
+++ src/index.js
@@ -3,5 +3,8 @@ import './index.scss';
 import code from './img/code.png'
 import { mult, sum } from './modules/calc';
 
-console.log(mult(3, 4));
-console.log(sum(3, 4));
+let button = document.querySelector('.header__menu-link');
+let menu = document.querySelector('.menu');
+button.onclick = () => {
+   menu.classList.toggle('menu--active');
+}
